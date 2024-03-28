420cb61e41ff43b2fd7593d7147f9dbe11b96de8
Khalid Khushal <khalid.centrox@gmail.com>
2024-03-28 05:12:39 +0500

    added selection and bubble sort alogorithms with js and python

--- sort/selection/selection.py
+++
@@ -1,9 +0,0 @@
-array = [1,2,4,3,6,5,1]
-
-for i in range(len(array)):
-    min_ind = i
-    for j in range(i+1,len(array)):
-        if (array[min_ind] > array[j]):
-            min_ind = j
-    array[i],array[min_ind] = array[min_ind],array[i]
-print(array)
